17d04d208950c9c4e2e8bc5251cf06205ec15285
Kuma <33801357+KumapowerLIU@users.noreply.github.com>
2020-11-07 20:47:02 +0800

    Update util.py

--- util/util.py
+++ util/util.py
@@ -73,14 +73,24 @@ def gussin(v):
     out = np.array(q)
     return torch.from_numpy(out)
 
-def cal_feat_mask(inMask, nlayers):
+def cal_feat_mask(inMask, conv_layers, threshold):
     assert inMask.dim() == 4, "mask must be 4 dimensions"
+    assert inMask.size(0) == 1, "the first dimension must be 1 for mask"
     inMask = inMask.float()
-    ntimes = 2**nlayers
-    inMask = F.interpolate(inMask, (inMask.size(2)//ntimes, inMask.size(3)//ntimes), mode='nearest')
-    inMask = inMask.detach().byte()
+    convs = []
+    inMask = Variable(inMask, requires_grad = False)
+    for id_net in range(conv_layers):
+        conv = nn.Conv2d(1,1,4,2,1, bias=False)
+        conv.weight.data.fill_(1/16)
+        convs.append(conv)
+    lnet = nn.Sequential(*convs)
+    if inMask.is_cuda:
 
-    return inMask
+        lnet = lnet.cuda()
+    output = lnet(inMask)
+    output = (output > threshold).float().mul_(1)
+
+    return output
 
 def cal_mask_given_mask_thred(img, mask, patch_size, stride, mask_thred):
     assert img.dim() == 3, 'img has to be 3 dimenison!'

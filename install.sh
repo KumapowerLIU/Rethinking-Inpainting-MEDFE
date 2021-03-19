a11e561d67990357a2343a5df8fcbd745385eedf
Mees van Loben Sels <mavanlobensels@gmail.com>
2021-03-19 15:20:09 +0100

    install script

---
+++ install.sh
@@ -0,0 +1,8 @@
+sudo apt install git
+sudo apt install python3-pip
+mkdir cs4240
+cd cs4240
+git clone https://github.com/mvanlobensels/Rethinking-Inpainting-MEDFE.git
+cd Rethinking-Inpainting-MEDFE
+pip install -r requirements.txt --no-cache-dir
+
